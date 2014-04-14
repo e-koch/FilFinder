e6b11d09b7531e60f46af989294aed47c9bd60ee
e-koch <koch.eric.w@gmail.com>
2014-04-13 22:23:22 -0700

    RHT computed using array multiplication to speed things up.

--- fil_finder/rollinghough.py
+++ fil_finder/rollinghough.py
@@ -20,33 +20,38 @@ def rht(mask, radius, ntheta=180, background_percentile=25):
 
     '''
 
-    circle, mesh = circular_region(radius)
     pad_mask = np.pad(mask.astype(float), radius, padwithnans)
     theta = np.linspace(0,np.pi, ntheta)
+
+
+    ## Create a cube of all angle positions
+    circle, mesh = circular_region(radius)
+    circles_cube = np.empty((ntheta, circle.shape[0], circle.shape[1]))
+    for posn, ang in enumerate(theta):
+        diff = mesh[0]*np.sin(ang) - mesh[1]*np.cos(ang)
+        diff[np.where(np.abs(diff)<1.0)] = 0
+        circles_cube[posn,:,:] = diff
 
     R = np.zeros((ntheta,))
     x, y = np.where(mask!=0.0)
     for i,j in zip(x,y):
-        region = circle * pad_mask[i:i+2*radius+1,j:j+2*radius+1]
-        for posn, ang in enumerate(theta):
-            diff = mesh[0]*np.sin(ang) - mesh[1]*np.cos(ang)
-            diff[np.where(np.abs(diff)<1.0)] = 0
+        region = np.tile(circle * pad_mask[i:i+2*radius+1,j:j+2*radius+1], (ntheta, 1, 1))
+        line = region * np.isclose(circles_cube, 0.0)
 
-            line = region * np.isclose(diff, 0.0)
+        if np.isnan(line).all():
+            pass ## If all nans, ignore
+        else:
+            R = R + np.nansum(np.nansum(line, axis=2), axis=1)
 
-            if np.isnan(line).all():
-                pass ## If all nans, ignore
-            else:
-                R[posn] += np.nansum(line)
     ## You're likely to get a somewhat constant background, so subtract that out
-    R = R - np.median(R[R<scoreatpercentile(R,background_percentile)])
+    R = R - np.median(R[R<=scoreatpercentile(R,background_percentile)])
     if (R<0.0).any():
         R[R<0.0] = 0.0 ## Ignore negative values after subtraction
-
     max_posn = np.where(R==R.max())[0]
     if max_posn.shape[0]>1:
         max_posn = int(np.mean(max_posn))
     theta = np.roll(theta, max_posn)
+    R = np.roll(R, max_posn)
 
     return theta, R
 
