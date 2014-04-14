Fixed weighting type in radial_profile. Added chi-squared function.

fil_finder/width.py
@@ -313,7 +313,7 @@ def radial_profile(img, dist_transform_all, dist_transform_sep, offsets,\
 	if weighting=="number":
 		weights = np.array([whichbins[whichbins==bin].sum() for bin in range(1,int(nbins)+1)])
 	elif weighting=="var":
-		weights = [np.nanvar(width_value[whichbins==bin]) for bin in range(1,int(nbins)+1)]
+		weights = np.array([np.nanvar(width_value[whichbins==bin]) for bin in range(1,int(nbins)+1)])
 		weights[np.isnan(weights)] = 0.0 # Empty bins
 
 	# Ignore empty bins