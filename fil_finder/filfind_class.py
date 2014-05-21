71263013421f716ca211a574e5d4d9009d4b4f9c
e-koch <koch.eric.w@gmail.com>
2014-05-21 15:01:50 -0700

    Updated analyze_skeletons to work with new length functions.

--- fil_finder/filfind_class.py
+++ fil_finder/filfind_class.py
@@ -429,23 +429,26 @@ class fil_finder_2D(object):
         interpts, hubs, ends, filbranches, labeled_fil_arrays =  \
                 pix_identify(isolated_filaments, num)
 
-        initial_lengths, branch_intensity = init_lengths(labeled_fil_arrays, filbranches, self.array_offsets, self.image)
+        branch_lengths, branch_intensity = init_lengths(labeled_fil_arrays, filbranches, self.array_offsets, self.image)
 
-        edge_list, nodes = pre_graph(labeled_fil_arrays, initial_lengths, branch_intensity, interpts, ends)
+        edge_list, nodes = pre_graph(labeled_fil_arrays, branch_lengths, branch_intensity, interpts, ends)
 
-        max_path, extremum, G = longest_path(edge_list, nodes, initial_lengths,
+        max_path, extremum, G = longest_path(edge_list, nodes, branch_lengths,
                                              verbose=verbose,
                                              skeleton_arrays=labeled_fil_arrays)
 
-        labeled_fil_arrays, edge_list, nodes = prune_graph(G, nodes, edge_list, max_path, labeled_fil_arrays, self.branch_thresh)
+        updated_lists = prune_graph(G, nodes, edge_list, max_path, labeled_fil_arrays, \
+                                    branch_lengths, filbranches, self.branch_thresh)
+
+        labeled_fil_arrays, edge_list, nodes, branch_lengths, filbranches = updated_lists
 
         self.filament_extents = extremum_pts(labeled_fil_arrays, extremum, ends)
 
-        main_lengths, branch_lengths, labeled_fil_arrays = \
-            final_lengths(self.image, max_path, edge_list, labeled_fil_arrays, filament_pixels, interpts, filbranches, \
-                            initial_lengths, self.imgscale, self.branch_thresh)
+        main_lengths = main_length(max_path, edge_list, labeled_fil_arrays, \
+                                   interpts, branch_lengths, self.imgscale, \
+                                   verbose=verbose)
 
-        labeled_fil_arrays, filbranches, final_hubs, branch_lengths, filament_arrays = final_analysis(labeled_fil_arrays)
+        # labeled_fil_arrays, filbranches, final_hubs, branch_lengths, filament_arrays = final_analysis(labeled_fil_arrays)
 
         self.lengths = main_lengths
         self.labelled_filament_arrays = labeled_fil_arrays
