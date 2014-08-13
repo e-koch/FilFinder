fdb3e3ac6e3481dedf21cae027570d23b1627a72
e-koch <koch.eric.w@gmail.com>
2014-08-13 11:28:21 -0700

    More doc testing..

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['matplotlib', 'astropy', 'scipy', 'scikits-image', 'numpy', 'pandas', "networkx"]
+MOCK_MODULES = ['matplotlib', 'astropy', 'scipy', 'scikits-image', 'numpy', 'pandas', "networkx", "fil_finder"]
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
