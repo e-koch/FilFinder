93d08f24d33d481c2170d61df28254990e582a35
e-koch <koch.eric.w@gmail.com>
2014-08-13 11:00:21 -0700

    Try to mock import requirements.

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,22 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 
+
+# Mock out the imports
+from mock import Mock as MagicMock
+
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['matplotlib', 'astropy', 'scipy', 'scikits-image', 'numpy', 'pandas', "networkx"]
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
+
+
+
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
