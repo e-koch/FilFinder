884f55039dfdf60919f938b487fe140dbf44e5ac
e-koch <koch.eric.w@gmail.com>
2015-01-19 15:23:10 -0700

    Proper setup script with setuptools bootstrap.

--- setup.py
+++ setup.py
@@ -1,13 +1,75 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from ez_setup import use_setuptools
+use_setuptools()
 
-setup(name='fil_finder',
-      version='1.0',
-      description='Python package for analysis of filamentary structure in molecular clouds.',
-      author='Eric Koch and Erik Rosolowsky',
-      author_email='koch.eric.w@gmail.com',
-      url='http://github.com/e-koch/fil_finder',
-      packages=['fil_finder'],
-      requires=['numpy','astropy','scipy','skimage','networkx','pandas']
-     )
+
+from setuptools import setup, find_packages
+from pkg_resources import parse_version
+
+
+def check_dependencies():
+
+    try:
+        import matplotlib
+        mpl_version = matplotlib.__version__
+        if parse_version(mpl_version) < parse_version('1.2'):
+            print("***Before installing, upgrade matplotlib to 1.2***")
+            raise ImportError
+    except:
+        raise ImportError(
+            "Install or upgrade matplotlib before installing fil_finder.")
+
+    try:
+        from numpy.version import version as np_version
+        if parse_version(np_version) < parse_version('1.6'):
+            print("***Before installing, upgrade numpy to 1.6***")
+            raise ImportError
+    except:
+        raise ImportError(
+            "Install or upgrade numpy before installing fil_finder.")
+
+    try:
+        from scipy.version import version as sc_version
+        if parse_version(sc_version) < parse_version('0.12'):
+            print("***Before installing, upgrade scipy to 0.12***")
+            raise ImportError
+    except:
+        raise ImportError(
+            "Install or upgrade scipy before installing fil_finder.")
+
+    try:
+        from astropy.version import version as ast_version
+        if parse_version(ast_version[:3]) < parse_version('0.2'):
+            print(("""***Before installing, upgrade astropy to 0.4.
+                    NOTE: This is the dev version as of 17/06/14.***"""))
+            raise ImportError("")
+    except:
+        raise ImportError(
+            "Install or upgrade astropy before installing fil_finder.")
+
+    try:
+        from networkx.version import version as nx_version
+    except:
+        raise ImportError(
+            "Install networkx before installing fil_finder.")
+
+    try:
+        import skimage
+    except:
+        raise ImportError(
+            "Install or upgrade skimage before installing fil_finder.")
+
+if __name__ == "__main__":
+
+    check_dependencies()
+
+    setup(name='fil_finder',
+          version='1.0',
+          description='Python package for analysis of filamentary structure in molecular clouds.',
+          author='Eric Koch and Erik Rosolowsky',
+          author_email='koch.eric.w@gmail.com',
+          url='http://github.com/e-koch/fil_finder',
+          packages=['fil_finder'],
+          requires=['numpy','astropy','scipy','skimage','networkx']
+         )
