4b217caef13236e170cc8839450f8d1669ac9381
e-koch <koch.eric.w@gmail.com>
2015-01-19 14:31:12 -0700

    Revert example code.

--- examples/new_test_script.py
+++ examples/new_test_script.py
@@ -1,14 +1,12 @@
 from fil_finder import fil_finder_2D
 from astropy.io.fits import getdata
 
-img, hdr = getdata("orionB-350.fits",
+img,hdr = getdata("chamaeleonI-250_normed.fits",
     header=True)
-
-img += 26.216 #85.452  #879.063
 
 # Utilize fil_finder_2D class
 # See filfind_class.py for inputs
-test = fil_finder_2D(img, hdr, 15.1, 30, 5, 50, distance=170., glob_thresh=20) #, region_slice=[580,1620,470,1120])
+test = fil_finder_2D(img, hdr, 15.1, 30, 5, 50, distance=170., glob_thresh=20, region_slice=[580,1620,470,1120])
 test.create_mask(verbose=True, border_masking=True)
 test.medskel(verbose=True)
 test.analyze_skeletons()
