98d8cc3233073ac1ab1327f7bf0836f3fe6f0b40
e-koch <koch.eric.w@gmail.com>
2015-02-19 10:39:26 -0700

    Clean-up of analysis script.

--- examples/paper_figures/bulk_analysis.py
+++ examples/paper_figures/bulk_analysis.py
@@ -85,37 +85,24 @@ if scatter == "T":
     scatter = True
 else:
     scatter = False
-# Make histograms
-hists = sys.argv[2]
-if hists == "T":
-    hists = True
-else:
-    hists = False
 # Make triangle plot
-triangle_plot = sys.argv[3]
+triangle_plot = sys.argv[2]
 if triangle_plot == "T":
     triangle_plot = True
 else:
     triangle_plot = False
 # Create KS Test tables
-ks_tests = sys.argv[4]
+ks_tests = sys.argv[3]
 if ks_tests == "T":
     ks_tests = True
 else:
     ks_tests = False
 # Compute the covering fraction
-covering_frac = sys.argv[5]
+covering_frac = sys.argv[4]
 if covering_frac == "T":
     covering_frac = True
 else:
     covering_frac = False
-
-# Stability analysis
-stability = sys.argv[6]
-if stability == "T":
-    stability = True
-else:
-    stability = False
 
 # Scatter plots
 if scatter:
@@ -167,20 +154,6 @@ if scatter:
     p.tight_layout()
     p.show()
 
-    # Orientation vs. Amplitude
-
-    # for i, key in enumerate(widths.keys()):
-    #     weights = median_bright[key]
-    #     # factor = 20/weights.min()
-    #     # weights *= factor
-    #     p.title(key)
-    #     p.scatter(orientation[key], (median_bright[key]), c=symb_col[i][0],
-    #               marker=symb_col[i][1], alpha=0.3)  # , s=weights)
-    #     p.grid(True)
-    #     p.xlabel(r"$\theta$")
-    #     p.ylabel(r"log$_{10}(I$/ MJy/sr)")
-    #     p.show()
-
     # Length vs Width
     lenmed = []
     lenstd = []
@@ -207,8 +180,8 @@ if scatter:
     p.ylabel("log$_{10}(W$/ pc)", fontsize=18)
     p.grid(True)
     p.legend(loc="lower right", ncol=2, prop={"size": 12}, markerscale=0.75)
-    p.ylim([-1.0, -0.05])
-    p.xlim([0.18, 1.6])
+    # p.ylim([-1.0, -0.05])
+    # p.xlim([0.18, 1.6])
     # fig.savefig("length_vs_width_centroids.eps", format="eps", dpi=1000)
     # p.clf()
     p.show()
@@ -229,62 +202,10 @@ if triangle_plot:
     figure = triangle.corner(data.T, labels=["log$_{10}$(W/ pc)",
                                              "log$_{10}$($I$/ MJy/sr)",
                                              "log$_{10}$(L/ pc)", r"$\delta$$\theta$", "$\theta$"],
-                             quantiles=[0.50, 0.85, 0.995], bins=9,
+                             quantiles=[0.15, 0.50, 0.85, 0.995], bins=9,
                              show_titles=False, title_args={"fontsize": 18})
     # figure.savefig('hgbs_scatter_hists.pdf', format='pdf', dpi=1000)
     p.show()
-
-# Histograms
-if hists:
-    pass
-
-# bins = np.linspace(0.0, 1.0, 50)
-# n, bins, patches = p.hist([widths[key] for key in widths.keys()], bins, stacked=True, color=colours, label=widths.keys())
-# p.legend()
-# p.ylabel("N")
-# p.xlabel(r"parsecs")
-# p.show()
-# all_widths = all_widths[all_widths<1.0]
-# print "Median Width %s" % (all_widths[all_widths<1.0].median().median())
-# print "IQR %s" %
-# ((all_widths[all_widths<1.0].quantile(.75)-all_widths[all_widths<1.0].quantile(.25)).mean())
-
-# lengths
-# medians = []
-# iqr = []
-# maximum = []
-# for key in lengths.keys():
-# medians.append(np.median(lengths[key]))
-# iqr.append(scoreatpercentile(lengths[key], 75) - scoreatpercentile(lengths[key], 25))
-# maximum.append(np.max(lengths[key]))
-
-# n, bins, patches = p.hist([lengths[key] for key in lengths.keys()], bins=200, stacked=True, color=colours, label=lengths.keys())
-# p.ylabel("N")
-# p.xlabel(r"parsecs")
-# p.legend()
-# p.show()
-
-# print "Median Length %s" % (np.median(medians))
-# print "IQR %s" % (np.mean(iqr))
-# print "Max Length %s" % (np.max(maximum))
-
-
-# Curvature
-# n, bins, patches = p.hist([curvature[key] for key in curvature.keys()], bins=50, stacked=True, color=colours, label=curvature.keys())
-# p.ylabel("N")
-# p.xlabel(r"$\theta$")
-# p.legend()
-# p.show()
-
-# medians = []
-# iqr = []
-# maximum = []
-# for key in curvature.keys():
-#     medians.append(np.median(curvature[key]))
-#     iqr.append(scoreatpercentile(curvature[key], 75) - scoreatpercentile(curvature[key], 25))
-#     maximum.append(np.max(curvature[key]))
-# print "Median Curvature %s" % (np.median(medians))
-# print "IQR %s" % (np.mean(iqr))
 
 if ks_tests:
 
@@ -400,26 +321,6 @@ if covering_frac:
 
     from pandas import DataFrame
     from astropy.io.fits import getdata
-    import astropy.units as u
-
-    def compute_cf(length, width, amplitude, background, image, header, distance):
-        '''
-        Compute a "covering fraction" which roughly describes the extent
-        that filaments cover in a region.
-        '''
-
-        cdelt2 = header["CDELT2"] * (u.degree)  # /u.pixel
-        conv = cdelt2 * (np.pi / 180.) * (1 / u.degree)
-        img_scale = distance * u.pc * conv
-
-        fil_cover = np.sum(length * width * (amplitude - background) /
-                           2. * (u.pc ** 2. * u.Jy / u.sr * 1e6) * img_scale ** -2.)
-        im_cover = np.nansum(image[image > 0.0]) * u.Jy / u.sr * 1e6
-
-        print fil_cover
-        print im_cover
-
-        return fil_cover / im_cover
 
     dist = [145., 175., 260., 400., 150., 170., 235.,
             460., 400., 400., 450., 450., 450.]  # pc
@@ -434,38 +335,7 @@ if covering_frac:
         model = getdata(name + "/" + name + "_filament_model.fits")
         cf[i] = np.nansum(model) / np.nansum(img)
         ordered_labels.append(labels[name])
-        # cf[i] = compute_cf(lengths[name], widths[name], amplitude[name], background[name], im, head, dist[i]).value
     df = DataFrame(cf, index=ordered_labels, columns=["Covering Fraction"])
     print(df)
     df.to_latex("covering_fracs.tex")
     df.to_csv("covering_fracs.csv")
-
-if stability:
-
-    from astropy.units import G
-
-    # Still need conversion to column densities??
-
-    # Divide by canonical cdens_0 value??
-    M_line = lambda cdens : 2*cdens / G
-
-    # Plotting width versus \lambda / \lambda_0
-    # Split - Orion + California against the rest
-    from seaborn import kdeplot
-
-    widths1 = np.empty((0))
-    widths2 = np.empty((0))
-    intens1 = np.empty((0))
-    intens2 = np.empty((0))
-
-    for key in widths.keys():
-        if key[0] == 'c':
-            widths1 = np.append(widths1, widths[key])
-            intens1 = np.append(intens1, amplitude[key])
-        else:
-            widths2 = np.append(widths2, widths[key])
-            intens2 = np.append(intens2, amplitude[key])
-
-    kdeplot(widths1, data2=intens1, shade=True)
-    kdeplot(widths2, data2=intens2, shade=True)
-    p.show()
