2d929cf8b67d45c400f366e2ddea2f862236eb80
e-koch <koch.eric.w@gmail.com>
2015-06-05 16:16:23 -0600

    Remove curvature from KS plot

--- examples/paper_figures/ks_plots.py
+++ examples/paper_figures/ks_plots.py
@@ -11,7 +11,7 @@ import numpy as np
 import seaborn as sn
 sn.set_context('talk')
 sn.set_style('ticks')
-sn.mpl.rc("figure", figsize=(7, 9))
+# sn.mpl.rc("figure", figsize=(7, 9))
 
 # Widths
 
@@ -27,13 +27,14 @@ widths_arr[np.arange(0, 14), np.arange(0, 14)] = 1.0
 widths_arr = -np.log10(widths_arr)
 
 # p.figure(figsize=(12, 10))
-p.subplot(211)
+p.subplot(111)
 # p.xlabel("Widths")
 p.imshow(widths_arr, origin='lower', cmap='binary', interpolation='nearest')
-p.xticks(np.arange(0, 14), [], rotation=90)
+p.xticks(np.arange(0, 14), widths.columns, rotation=90)
+# p.xticks(np.arange(0, 14), [], rotation=90)
 p.yticks(np.arange(0, 14), widths.columns)
 
-p.figtext(0.05, 0.95, "a)", fontsize=20)
+# p.figtext(0.05, 0.95, "a)", fontsize=20)
 
 cb = p.colorbar()
 cb.set_label(r'$-\log_{10}$ p-value')
@@ -43,30 +44,30 @@ p.tight_layout()
 p.show()
 # Curvature
 
-curve = read_csv("curvature_branches_ks_table_pvals.csv")
+# curve = read_csv("curvature_ks_table_pvals.csv")
 
-curve.index = curve["Unnamed: 0"]
-del curve["Unnamed: 0"]
+# curve.index = curve["Unnamed: 0"]
+# del curve["Unnamed: 0"]
 
-curve_arr = np.asarray(curve)
+# curve_arr = np.asarray(curve)
 
-curve_arr[np.arange(0, 14), np.arange(0, 14)] = 1.0
+# curve_arr[np.arange(0, 14), np.arange(0, 14)] = 1.0
 
-curve_arr = -np.log10(curve_arr)
+# curve_arr = -np.log10(curve_arr)
 
-# p.figure(figsize=(12, 10))
-p.subplot(212)
-# p.xlabel("Curvature")
-p.imshow(curve_arr, interpolation='nearest', origin='lower', cmap='binary')
-p.xticks(np.arange(0, 14), curve.columns, rotation=90)
-p.yticks(np.arange(0, 14), curve.columns)
+# # p.figure(figsize=(12, 10))
+# p.subplot(212)
+# # p.xlabel("Curvature")
+# p.imshow(curve_arr, interpolation='nearest', origin='lower', cmap='binary')
+# p.xticks(np.arange(0, 14), curve.columns, rotation=90)
+# p.yticks(np.arange(0, 14), curve.columns)
 
-p.figtext(0.05, 0.55, "b)", fontsize=20)
+# p.figtext(0.05, 0.55, "b)", fontsize=20)
 
-cb = p.colorbar()
-cb.set_label(r'$-\log_{10}$ p-value')
-cb.solids.set_edgecolor("face")
+# cb = p.colorbar()
+# cb.set_label(r'$-\log_{10}$ p-value')
+# cb.solids.set_edgecolor("face")
 
-p.tight_layout()
+# p.tight_layout()
 
-p.show()
+# p.show()
