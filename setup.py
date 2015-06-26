a82fabaf8c75eb3722d94d1836ae25c75e334456
e-koch <koch.eric.w@gmail.com>
2015-06-26 17:05:17 -0600

    Give more info when setup installs

--- setup.py
+++ setup.py
@@ -36,55 +36,69 @@ class check_deps(_build_ext):
 
 def check_dependencies():
 
+    def print_fail():
+        return "fil_finder will not install dependencies. Install checks" \
+               " if they are available, and proceeds if they are."
+
     try:
         from numpy.version import version as np_version
         if parse_version(np_version) < parse_version('1.7'):
             print("***Before installing, upgrade numpy to 1.7***")
+            print_fail()
             raise ImportError
     except:
         raise ImportError(
-            "Install or upgrade numpy before installing fil_finder.")
+            "Install or upgrade numpy before installing fil_finder." +
+            print_fail())
 
     try:
         from scipy.version import version as sc_version
         if parse_version(sc_version) < parse_version('0.12'):
             print("***Before installing, upgrade scipy to 0.12***")
+            print_fail()
             raise ImportError
     except:
         raise ImportError(
-            "Install or upgrade scipy before installing fil_finder.")
+            "Install or upgrade scipy before installing fil_finder." +
+            print_fail())
 
     try:
         import matplotlib
         mpl_version = matplotlib.__version__
         if parse_version(mpl_version) < parse_version('1.2'):
             print("***Before installing, upgrade matplotlib to 1.2***")
+            print_fail()
             raise ImportError
     except:
         raise ImportError(
-            "Install or upgrade matplotlib before installing fil_finder.")
+            "Install or upgrade matplotlib before installing fil_finder." +
+            print_fail())
 
     try:
         from astropy.version import version as ast_version
         if parse_version(ast_version[:3]) < parse_version('0.2'):
             print(("""***Before installing, upgrade astropy to 0.2.
                     NOTE: This is the dev version as of 17/06/14.***"""))
+            print_fail()
             raise ImportError("")
     except:
         raise ImportError(
-            "Install or upgrade astropy before installing fil_finder.")
+            "Install or upgrade astropy before installing fil_finder." +
+            print_fail())
 
     try:
         from networkx.version import version as nx_version
     except:
         raise ImportError(
-            "Install networkx before installing fil_finder.")
+            "Install networkx before installing fil_finder." +
+            print_fail())
 
     try:
         import skimage
     except:
         raise ImportError(
-            "Install or upgrade skimage before installing fil_finder.")
+            "Install or upgrade skimage before installing fil_finder." +
+            print_fail())
 
 if __name__ == "__main__":
 
