7f31f423b2a7e952cebb333d0162ab5ae463f687
e-koch <koch.eric.w@gmail.com>
2015-09-04 13:59:49 -0600

    Set pad sizes

--- fil_finder/tests/test_whole.py
+++ fil_finder/tests/test_whole.py
@@ -16,7 +16,8 @@ class Test_FilFinder(TestCase):
 
         test1 = fil_finder_2D(img, hdr, 10.0, flatten_thresh=95,
                               distance=260, size_thresh=430,
-                              glob_thresh=20, save_name="test1")
+                              glob_thresh=20, save_name="test1",
+                              pad_size=10, skeleton_pad_size=10)
 
         test1.create_mask(border_masking=False)
         test1.medskel()
@@ -52,7 +53,8 @@ class Test_FilFinder(TestCase):
 
         test2 = fil_finder_2D(img, hdr, 10.0, flatten_thresh=95,
                               distance=260, size_thresh=430,
-                              glob_thresh=20, save_name="test2")
+                              glob_thresh=20, save_name="test2",
+                              pad_size=10, skeleton_pad_size=10)
 
         test2.create_mask(border_masking=False)
         test2.medskel()
