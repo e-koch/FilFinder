1e5049c0393fe462281b2ccca3dc3021d873929a
e-koch <koch.eric.w@gmail.com>
2015-11-19 10:15:06 -0700

    Clean-up package imports

--- fil_finder/__init__.py
+++ fil_finder/__init__.py
@@ -2,11 +2,5 @@
 
 __version__ = "1.2.2"
 
-# from cores import *
-from length import *
-from pixel_ident import *
-from utilities import *
-from width import *
-
-from analysis import Analysis
-from filfind_class import fil_finder_2D
+from .analysis import Analysis
+from .filfind_class import fil_finder_2D
