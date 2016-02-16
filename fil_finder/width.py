05c60f2d1526c567eb1ed5f077f69ac55135083a
e-koch <koch.eric.w@gmail.com>
2016-02-16 15:08:19 -0700

    Default width guess to the std

--- fil_finder/width.py
+++ fil_finder/width.py
@@ -140,7 +140,7 @@ def gauss_model(distance, rad_profile, weights, img_beam):
         Identifies a failed fit.
     '''
 
-    p0 = (np.max(rad_profile), 0.1, np.min(rad_profile))
+    p0 = (np.max(rad_profile), np.std(rad_profile), np.min(rad_profile))
     parameters = ["Amplitude", "Width", "Background", "FWHM"]
 
     def gaussian(x, *p):
