7bb81c20db392cff673af3b65d6d80475c4af3c0
e-koch <koch.eric.w@gmail.com>
2016-08-17 10:17:41 +0200

    Simple gaussian test

--- fil_finder/tests/test_widths.py
+++ fil_finder/tests/test_widths.py
@@ -1,5 +1,5 @@
 
-from fil_finder.width import nonparam_width
+from fil_finder.width import nonparam_width, gauss_model
 
 import numpy as np
 import numpy.testing as npt
@@ -9,13 +9,30 @@ def test_nonparam():
 
     pts = np.linspace(0, 10, 100)
 
-    profile = 2.0*np.exp(-pts**2 / (2*3.0**2)) + 0.5
+    profile = 2.0 * np.exp(-pts ** 2 / (2 * 3.0 ** 2)) + 0.5
 
     params, errors, fail = \
         nonparam_width(pts, profile, pts, profile, 1.0, 5, 99)
 
     # This shouldn't be failing
     assert fail is False
+
+    # Check the amplitude
+    npt.assert_allclose(params[0], 2.5, atol=0.01)
+    # Width
+    npt.assert_allclose(params[1], 3.0, atol=0.01)
+    # Background
+    npt.assert_allclose(params[2], 0.5, atol=0.02)
+
+
+def test_gaussian():
+
+    pts = np.linspace(0, 10, 100)
+
+    profile = 2.0 * np.exp(-pts ** 2 / (2 * 3.0 ** 2)) + 0.5
+
+    params, errors, _, _, fail = \
+        gauss_model(pts, profile, np.ones_like(pts), 1.0)
 
     # Check the amplitude
     npt.assert_allclose(params[0], 2.5, atol=0.01)
