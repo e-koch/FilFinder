80e77f6275545196a8ce23007515f5b2f2fa93b6
e-koch <koch.eric.w@gmail.com>
2017-11-28 09:44:53 -0700

    Preparing v1.5 release

--- setup.py
+++ setup.py
@@ -123,7 +123,7 @@ if __name__ == "__main__":
     builtins._ASTROPY_PACKAGE_NAME_ = PACKAGENAME
 
     # VERSION should be PEP386 compatible (http://www.python.org/dev/peps/pep-0386)
-    VERSION = '1.5.dev'
+    VERSION = '1.5'
 
     # Indicates if this version is a release version
     RELEASE = 'dev' not in VERSION
