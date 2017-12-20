a2680e1fd80564dca295e743d3bdf2ac51f254ff
e-koch <koch.eric.w@gmail.com>
2017-12-19 16:17:02 -0800

    Use numpy testing functions; add explicit testing of the mask and skeleton arrays

--- fil_finder/tests/test_whole.py
+++ fil_finder/tests/test_whole.py
@@ -7,7 +7,7 @@ import numpy.testing as npt
 import astropy.units as u
 from copy import deepcopy
 
-from fil_finder import fil_finder_2D
+from .. import fil_finder_2D
 
 from _testing_data import *
 
@@ -28,6 +28,10 @@ class Test_FilFinder(TestCase):
         test1.find_widths()
         test1.compute_filament_brightness()
 
+
+        assert ((mask1 > 0) == test1.mask).all()
+        assert ((skeletons1 > 0) == test1.skeleton).all()
+
         assert test1.number_of_filaments == len(table1["Lengths"])
 
         for i, param in enumerate(test1.width_fits["Names"]):
@@ -37,19 +41,19 @@ class Test_FilFinder(TestCase):
                                 np.asarray(table1[param + " Error"]),
                                 rtol=1e-4)
 
-        assert np.allclose(test1.lengths,
-                           np.asarray(table1['Lengths']))
+        npt.assert_allclose(test1.lengths,
+                            table1['Lengths'].quantity.value)
 
         assert (test1.width_fits['Type'] == table1['Fit Type']).all()
 
-        assert np.allclose(test1.total_intensity,
-                           np.asarray(table1['Total Intensity']))
+        npt.assert_allclose(test1.total_intensity,
+                            table1['Total Intensity'].quantity.value)
 
-        assert np.allclose(test1.filament_brightness,
-                           np.asarray(table1['Median Brightness']))
+        npt.assert_allclose(test1.filament_brightness,
+                            table1['Median Brightness'].quantity.value)
 
-        assert np.allclose(test1.branch_properties["number"],
-                           np.asarray(table1['Branches']))
+        npt.assert_allclose(test1.branch_properties["number"],
+                            table1['Branches'].quantity.value)
 
     def test_without_rht_branches(self):
         # Non-branches
@@ -66,33 +70,38 @@ class Test_FilFinder(TestCase):
         test2.find_widths()
         test2.compute_filament_brightness()
 
+        assert ((mask2 > 0) == test2.mask).all()
+        assert ((skeletons2 > 0) == test2.skeleton).all()
+
         assert test2.number_of_filaments == len(table2["Lengths"])
 
         for i, param in enumerate(test2.width_fits["Names"]):
             npt.assert_allclose(test2.width_fits["Parameters"][:, i],
                                 np.asarray(table2[param]), rtol=1e-4)
             npt.assert_allclose(test2.width_fits["Errors"][:, i],
-                                np.asarray(table2[param + " Error"]), rtol=1e-4)
+                                np.asarray(table2[param + " Error"]),
+                                rtol=1e-4)
 
-        assert np.allclose(test2.lengths,
-                           np.asarray(table2['Lengths']))
+        npt.assert_allclose(test2.lengths,
+                            table2['Lengths'].quantity.value)
 
         assert (test2.width_fits['Type'] == table2['Fit Type']).all()
 
-        assert np.allclose(test2.total_intensity,
-                           np.asarray(table2['Total Intensity']))
+        npt.assert_allclose(test2.total_intensity,
+                            table2['Total Intensity'].quantity.value)
 
-        assert np.allclose(test2.filament_brightness,
-                           np.asarray(table2['Median Brightness']))
+        npt.assert_allclose(test2.filament_brightness,
+                            table2['Median Brightness'].quantity.value)
 
-        assert np.allclose(test2.branch_properties["number"],
-                           np.asarray(table2['Branches']))
+        npt.assert_allclose(test2.branch_properties["number"],
+                            table2['Branches'].quantity.value)
 
-        assert np.allclose(test2.rht_curvature['Orientation'],
-                           np.asarray(table2['Orientation']))
+        npt.assert_allclose(test2.rht_curvature['Orientation'],
+                            table2['Orientation'].quantity.value)
 
-        assert np.allclose(test2.rht_curvature['Curvature'],
-                           np.asarray(table2['Curvature']))
+        npt.assert_allclose(test2.rht_curvature['Curvature'],
+                            table2['Curvature'].quantity.value,
+                            atol=5e-3)
 
     def test_equal_branches(self):
         '''
