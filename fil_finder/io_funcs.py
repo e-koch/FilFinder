0403cf3b138f6e79a0242a2a3e909435fb56d39a
e-koch <koch.eric.w@gmail.com>
2018-01-10 10:49:51 -0700

    Revamp loading data for more input types and attach units

--- fil_finder/io_funcs.py
+++ fil_finder/io_funcs.py
@@ -1,12 +1,22 @@
 # Licensed under an MIT open source license - see LICENSE
 
 from astropy.io.fits import PrimaryHDU
-# from spectral_cube.lower_dimensional_structures import LowerDimensionalObject
 import numpy as np
+import astropy.units as u
+import re
+import warnings
+
+try:
+    from spectral_cube import Projection, Slice
+    SPECTRALCUBE_INSTALL = True
+except ImportError:
+    SPECTRALCUBE_INSTALL = False
 
 
-allowed_types = ["numpy.ndarray", "astropy.io.fits.PrimaryHDU"]  # ,
-                 # "spectral_cube.LowerDimensionalObject"]
+allowed_types = ["numpy.ndarray", "astropy.io.fits.PrimaryHDU",
+                 "spectral_cube.Projection", 'spectral_cube.Slice',
+                 "astropy.units.Quantity"]
+
 
 def input_data(data):
     '''
@@ -29,17 +39,64 @@ def input_data(data):
     '''
 
     if isinstance(data, PrimaryHDU):
-        output_data = {"data": data.data.squeeze(), "header": data.header}
+        squeeze_data = data.data.squeeze()
+        if 'BUNIT' in data.header:
+            unit = convert_bunit(data.header['BUNIT'])
+        else:
+            unit = u.dimensionless_unscaled
+        output_data = {"data": squeeze_data * unit,
+                       "header": data.header}
 
-    # elif isinstance(data, LowerDimensionalObject):
-    #     output_data = (data.value, data.header)
-    elif isinstance(data, np.ndarray):
-        output_data = {"data": data.squeeze()}
+    elif isinstance(data, Projection) or isinstance(data, Slice):
+        # spectral-cube has dimensionality checks
+        output_data = (data, data.header)
+
+    elif isinstance(data, np.ndarray) or isinstance(data, u.Quantity):
+        squeeze_data = data.data.squeeze()
+        if not hasattr(data, 'unit'):
+            unit = u.dimensionless_unscaled
+            squeeze_data = squeeze_data * unit
+        output_data = {"data": squeeze_data}
     else:
         raise TypeError("Input data is not of an accepted form:"
-                        " astropy.io.fits.PrimaryHDU, np.ndarray.")
+                        "{}".format(allowed_types))
 
-    if len(output_data["data"].shape) != 2:
+    if dim_check(output_data["data"]):
         raise TypeError("Data must be 2D. Please re-check the inputs.")
 
     return output_data
+
+
+def dim_check(arr):
+    return True if arr.ndim == 2 else False
+
+
+def convert_bunit(bunit):
+    '''
+    Convert a BUNIT string to a quantity
+
+    Stolen from spectral-cube: https://github.com/radio-astro-tools/spectral-cube/blob/c6ef0e6b1f8ba54116c8f558466aff4efb8caf60/spectral_cube/cube_utils.py#L423
+
+    Parameters
+    ----------
+    bunit : str
+        String to convert to an `~astropy.units.Unit`
+
+    Returns
+    -------
+    unit : `~astropy.unit.Unit`
+        Corresponding unit.
+    '''
+
+    # special case: CASA (sometimes) makes non-FITS-compliant jy/beam headers
+    bunit_lower = re.sub("\s", "", bunit.lower())
+    if bunit_lower == 'jy/beam':
+        unit = u.Jy / u.beam
+    else:
+        try:
+            unit = u.Unit(bunit)
+        except ValueError:
+            warnings.warn("Could not parse unit {0}".format(bunit))
+            unit = None
+
+    return unit
