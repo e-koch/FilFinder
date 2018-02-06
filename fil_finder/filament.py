2e30ff043f1912af29c068d8145f1b3503a25fda
e-koch <koch.eric.w@gmail.com>
2018-02-06 13:19:41 -0700

    Working RHT analysis for Filament2D

--- fil_finder/filament.py
+++ fil_finder/filament.py
@@ -2,12 +2,15 @@
 
 import numpy as np
 import astropy.units as u
+import networkx as nx
+import warnings
 
 from .length import (init_lengths, main_length, make_final_skeletons,
                      pre_graph, longest_path, prune_graph)
 from .pixel_ident import pix_identify
 from .utilities import pad_image
 from .base_conversions import UnitConverter
+from .rollinghough import rht
 
 
 class FilamentNDBase(object):
@@ -152,6 +155,8 @@ class Filament2D(FilamentNDBase):
         # Must have a pad size of 1 for the morphological operations.
         pad_size = 1
 
+        branch_thresh = self._converter.to_pixel(branch_thresh)
+
         interpts, hubs, ends, filbranches, labeled_mask =  \
             pix_identify([self.skeleton(pad_size=pad_size)], 1)
 
@@ -172,6 +177,9 @@ class Filament2D(FilamentNDBase):
         branch_properties = init_lengths(labeled_mask, filbranches,
                                          [[(0, 0), (0, 0)]],
                                          input_image)
+
+        from copy import deepcopy
+        self._orig_branch_properties = deepcopy(branch_properties)
 
         # Add the number of branches onto the dictionary
         branch_properties["number"] = filbranches
@@ -226,25 +234,33 @@ class Filament2D(FilamentNDBase):
         self._branch_properties = \
             {'length': branch_properties['length'][0] * u.pix,
              'intensity': np.array(branch_properties['intensity'][0]),
-             'number': branch_properties['number'][0]}
+             'number': branch_properties['number'][0],
+             'pixels': branch_properties['pixels'][0]}
 
     @property
     def branch_properties(self):
         return self._branch_properties
 
     @property
-    def intersec_pts(self):
+    def branch_pts(self):
         '''
-        Skeleton pixels associated intersections.
+        Pixels within each skeleton branch.
         '''
-        return self.pixel_coords[self._intersec_idx]
+        return self.branch_properties['pixels']
 
-    @property
-    def end_pts(self):
-        '''
-        Skeleton pixels associated branch end.
-        '''
-        return self.pixel_coords[self._ends_idx]
+    # @property
+    # def intersec_pts(self):
+    #     '''
+    #     Skeleton pixels associated intersections.
+    #     '''
+    #     return self.pixel_coords[self._intersec_idx]
+
+    # @property
+    # def end_pts(self):
+    #     '''
+    #     Skeleton pixels associated branch end.
+    #     '''
+    #     return self.pixel_coords[self._ends_idx]
 
     def length(self, unit=u.pixel):
         return self._converter.from_pixel(self._length, unit)
@@ -263,14 +279,206 @@ class Filament2D(FilamentNDBase):
         '''
         return self._graph
 
-    def plot_graph(self, save_name=None):
-        pass
+    def plot_graph(self, save_name=None, layout_func=nx.spring_layout):
+        '''
+        Show the graph structure
+        '''
+        import matplotlib.pyplot as plt
 
-    def orientation_analysis(self):
+        G = self.graph
+
+        elist = [(u, v) for (u, v, d) in G.edges(data=True)]
+        posns = layout_func(G)
+
+        nx.draw_networkx_nodes(G, posns, node_size=200)
+        nx.draw_networkx_edges(G, posns, edgelist=elist, width=2)
+        nx.draw_networkx_labels(G, posns, font_size=10,
+                                font_family='sans-serif')
+        plt.axis('off')
+
+        if save_name is not None:
+            # Save the plot
+            pass
+
+        plt.draw()
+
+        # Add in the ipynb checker
+
+    def rht_analysis(self, radius=10 * u.pix, ntheta=180,
+                     background_percentile=25):
         '''
-        Use the RHT to find the filament orientation and dispersion.
+        Use the RHT to find the filament orientation and dispersion of the
+        longest path.
+
+        Parameters
+        ----------
+        radius : `~astropy.units.Quantity`, optional
+            Radius of the region to compute the orientation within. Converted
+            to pixel units and rounded to the nearest integer.
         '''
-        pass
+
+        if not hasattr(radius, 'unit'):
+            warnings.warn("Radius has no given units. Assuming pixel units.")
+            radius *= u.pix
+
+        radius = int(round(self._converter.to_pixel(radius).value))
+
+        longpath_arr = self.skeleton(out_type='longpath')
+
+        longpath_arr = np.fliplr(longpath_arr)
+
+        theta, R, quant = rht(longpath_arr, radius, ntheta,
+                              background_percentile)
+
+        twofive, mean, sevenfive = quant
+
+        self._orientation = mean * u.rad
+
+        if sevenfive > twofive:
+            self._curvature = np.abs(sevenfive - twofive) * u.rad
+        else:
+            self._curvature = (np.abs(sevenfive - twofive) + np.pi) * u.rad
+
+        self._orientation_hist = [theta, R]
+        self._orientation_quantiles = [twofive, sevenfive]
+
+    @property
+    def orientation_hist(self):
+        '''
+        Distribution of orientations from the RHT along the longest path.
+
+        Contains the angles of the distribution bins and the values in those
+        bins.
+        '''
+        return self._orientation_hist
+
+    @property
+    def orientation(self):
+        '''
+        Mean orientation of the filament along the longest path.
+        '''
+        return self._orientation
+
+    @property
+    def curvature(self):
+        '''
+        Interquartile range of the RHT orientation distribution along the
+        longest path.
+        '''
+        return self._curvature
+
+    def plot_rht_distrib(self, save_name=None):
+        '''
+        Plot the RHT distribution from `Filament2D.rht_analysis`.
+        '''
+
+        theta = self.orientation_hist[0]
+        R = self.orientation_hist[1]
+
+        import matplotlib.pyplot as plt
+
+        median = self.orientation
+        twofive, sevenfive = self._orientation_quantiles
+
+        ax1 = plt.subplot(121, polar=True)
+        ax1.plot(2 * theta, R / R.max(), "kD")
+        ax1.fill_between(2 * theta, 0,
+                         R[:, 0] / R.max(),
+                         facecolor="blue",
+                         interpolate=True, alpha=0.5)
+        ax1.set_rmax(1.0)
+        ax1.plot([2 * median] * 2, np.linspace(0.0, 1.0, 2), "g")
+        ax1.plot([2 * twofive] * 2, np.linspace(0.0, 1.0, 2),
+                 "b--")
+        ax1.plot([2 * sevenfive] * 2, np.linspace(0.0, 1.0, 2),
+                 "b--")
+        plt.subplot(122)
+        plt.imshow(self.skeleton(out_type='longpath'),
+                   cmap="binary", origin="lower")
+        if save_name is not None:
+            plt.savefig(save_name)
+        plt.show()
+
+    def rht_branch_analysis(self, radius=10 * u.pix, ntheta=180,
+                            background_percentile=25,
+                            min_branch_length=3 * u.pix,
+                            verbose=False, save_png=False):
+        '''
+        Use the RHT to find the filament orientation and dispersion of each
+        branch in the filament.
+
+        Parameters
+        ----------
+        '''
+
+        # Convert length cut to pixel units
+
+        if not hasattr(radius, 'unit'):
+            warnings.warn("Radius has no given units. Assuming pixel units.")
+            radius *= u.pix
+
+        if not hasattr(min_branch_length, 'unit'):
+            warnings.warn("min_branch_length has no given units. Assuming "
+                          "pixel units.")
+            min_branch_length *= u.pix
+
+        radius = int(round(self._converter.to_pixel(radius).value))
+        min_branch_length = self._converter.to_pixel(min_branch_length).value
+
+        means = []
+        iqrs = []
+
+        # Make padded arrays from individual branches
+        for i, (pix, length) in enumerate(zip(self.branch_pts,
+                                              self.branch_properties['length'])):
+
+            if length.value < min_branch_length:
+                means.append(np.NaN)
+                iqrs.append(np.NaN)
+                continue
+
+            # Setup size of array
+            ymax = pix[:, 0].max()
+            ymin = pix[:, 0].min()
+            xmax = pix[:, 1].max()
+            xmin = pix[:, 1].min()
+
+            shape = (ymax - ymin + 1 + 2 * radius,
+                     xmax - xmin + 1 + 2 * radius)
+
+            branch_array = np.zeros(shape, dtype=bool)
+            branch_array[pix[:, 0] - ymin + radius,
+                         pix[:, 1] - xmin + radius] = True
+
+            branch_array = np.fliplr(branch_array)
+
+            theta, R, quant = rht(branch_array, radius, ntheta,
+                                  background_percentile)
+
+            twofive, mean, sevenfive = quant
+
+            means.append(mean)
+            if sevenfive > twofive:
+                iqrs.append(np.abs(sevenfive - twofive))
+            else:
+                iqrs.append(np.abs(sevenfive - twofive) + np.pi)
+
+        self._orientation_branches = np.array(means) * u.rad
+        self._curvature_branches = np.array(iqrs) * u.rad
+
+    @property
+    def orientation_branches(self):
+        '''
+        Orientations along each branch in the filament.
+        '''
+        return self._orientation_branches
+
+    @property
+    def curvature_branches(self):
+        '''
+        Curvature along each branch in the filament.
+        '''
+        return self._curvature_branches
 
     def width_analysis(self):
         pass
