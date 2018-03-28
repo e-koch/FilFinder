83486ddce96e5fa828b6739cdeadfc2ee2464d20
e-koch <koch.eric.w@gmail.com>
2018-03-28 12:15:10 -0600

    Add top level imports

--- fil_finder/__init__.py
+++ fil_finder/__init__.py
@@ -5,3 +5,5 @@ from ._astropy_init import *
 if not _ASTROPY_SETUP_:
     from .filfind_class import fil_finder_2D
     from .filfinder2D import FilFinder2D
+    from .filament import FilamentNDBase, Filament2D
+    from .width_profiles import filament_profile
