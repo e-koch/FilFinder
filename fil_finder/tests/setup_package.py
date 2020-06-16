c10e32335bb895a82376850ad5f87d5505442d83
e-koch <koch.eric.w@gmail.com>
2020-06-16 16:29:43 -0600

    Updates for new infrastructure

--- fil_finder/tests/setup_package.py
+++ fil_finder/tests/setup_package.py
@@ -1,6 +1,5 @@
 def get_package_data():
     return {
-        _ASTROPY_PACKAGE_NAME_ + '.tests': ['testing_data/*.fits',
-                                            'testing_data/test1/*',
-                                            'testing_data/test2/*',
-                                            'coveragerc']}
+        _ASTROPY_PACKAGE_NAME_ + '.tests': ['data/*.fits',
+                                            'data/*.hdf5',
+                                            ]}
