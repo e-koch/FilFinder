cb4d8ffba729b4169a8bd0432b5af5aaf894c75c
e-koch <koch.eric.w@gmail.com>
2024-01-23 08:46:03 -0500

    Update _astropy_init

--- fil_finder/__init__.py
+++ fil_finder/__init__.py
@@ -1,9 +1,8 @@
 # Licensed under an MIT open source license - see LICENSE
 
-from ._astropy_init import *
+from ._astropy_init import __version__, test
 
-if not _ASTROPY_SETUP_:
-    from .filfind_class import fil_finder_2D
-    from .filfinder2D import FilFinder2D
-    from .filament import FilamentNDBase, Filament2D
-    from .width_profiles import filament_profile
+from .filfind_class import fil_finder_2D
+from .filfinder2D import FilFinder2D
+from .filament import FilamentNDBase, Filament2D
+from .width_profiles import filament_profile
