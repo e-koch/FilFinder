fa13ef19dc97975b156ba69374c00e75219c1a40
e-koch <koch.eric.w@gmail.com>
2024-04-19 11:37:59 -0400

    Remove old legacy filfinder version

--- fil_finder/__init__.py
+++ fil_finder/__init__.py
@@ -2,7 +2,6 @@
 
 from ._astropy_init import __version__, test
 
-from .filfind_class import fil_finder_2D
 from .filfinder2D import FilFinder2D
 from .filament import FilamentNDBase, Filament2D
 from .width_profiles import filament_profile
