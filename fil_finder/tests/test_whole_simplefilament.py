957c39e2bbe3ca7cf581516a74c49005bb3c9d68
Eric Koch <koch.eric.w@gmail.com>
2025-05-09 20:55:09 -0400

    Catch annoying +/-1 pixel variation in the simple case

--- fil_finder/tests/test_whole_simplefilament.py
+++ fil_finder/tests/test_whole_simplefilament.py
@@ -54,13 +54,21 @@ def test_simple_filament_noheader(simple_filament_model):
     assert len(branch_pts) == 1
     # Padded by 1 for morphological operations
     assert (branch_pts[0][:, 0] == 1).all()
-    try:
-        assert branch_pts[0][:, 1].shape[0] == 151
-        assert (branch_pts[0][:, 1] == np.arange(1, 152)).all()
-    except AssertionError:
-        # This handles an edge case where the medskel output varies by 1 pixel
+
+    # This handles an edge case where the medskel output varies by 1 pixel at each end
+    sum_branch = branch_pts[0][:, 0].sum()
+
+    if sum_branch == 150:
         assert branch_pts[0][:, 1].shape[0] == 150
         assert (branch_pts[0][:, 1] == np.arange(1, 151)).all()
+    elif sum_branch == 151:
+        assert branch_pts[0][:, 1].shape[0] == 151
+        assert (branch_pts[0][:, 1] == np.arange(1, 152)).all()
+    elif sum_branch == 152:
+        assert branch_pts[0][:, 1].shape[0] == 152
+        assert (branch_pts[0][:, 1] == np.arange(1, 153)).all()
+    else:
+        raise ValueError(f"Unexpected branch length {sum_branch}")
 
     # Image coordinate branch pts should match the skeleton
     skel_pts = np.where(test.skeleton)
