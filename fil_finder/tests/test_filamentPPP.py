86d4c465a76bf1b95e7f79c070c3a328cc1dfc4c
Eric Koch <koch.eric.w@gmail.com>
2025-05-12 13:00:48 -0400

    Fix xfail call

--- fil_finder/tests/test_filamentPPP.py
+++ fil_finder/tests/test_filamentPPP.py
@@ -97,7 +97,8 @@ def test_FilamentPPP():
     os.remove('pickled_fil.pkl')
 
 
-@pytest.mark.xfail("Known failing case. To be fixed in future release.")
+# TODO: This test is known to fail.
+@pytest.mark.xfail(reason="Known failing case. To be fixed in future release.")
 def test_FilamentPPP_onebranch():
 
     pixels = (np.array([0, 1, 2, 3, 2, 2, 2]),
